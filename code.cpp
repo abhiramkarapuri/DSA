7b0941761b5c82fe9ab44220c00ccfa2dcb7da88
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-10-23 22:34:31 +0530

    added some code

--- code.cpp
+++ code.cpp
@@ -2103,3 +2103,50 @@ using namespace std;
 // }
 
 //SEARCH IN A 2D MATRIX
+// bool searchInRow(vector<vector<int>>& mat, int target,int row){
+//         int n= mat[0].size();
+//         int st =0, end = n-1;
+//         while(st<=end){
+//             int mid = (st+end)/2;
+//             if(target == mat[row][mid]){
+//                 return true;
+//             }else if(target >=mat[row][mid]){
+//                 st = mid+1;
+//             }else{
+//                 end = mid-1;
+//             }
+//         }return false;
+//     }
+//     bool searchMatrix(vector<vector<int>>& mat, int target) {
+//         //BS on total rows
+//         int m = mat.size(), n= mat[0].size();
+//         int stRow =0,endRow = m-1;
+//         while(stRow<=endRow){
+//             int midRow = (stRow+endRow)/2;
+//             if(target >=mat[midRow][0] && target <=mat[midRow][n-1]){
+//                 // found the row => BS on this row
+//                 return searchInRow(mat,target,midRow);
+//             }else if(target >= mat[midRow][n-1]){
+//                 //down => right
+//                 stRow = midRow+1;
+//             }else{
+//                 // up => left
+//                 endRow = midRow-1;
+//             }
+//         }return false;
+//     }
+
+//SEARCH IN A 2D MATRIX ||
+// bool searchMatrix(vector<vector<int>>& mat, int target) {
+//         int m = mat.size(),n = mat[0].size();
+//         int r=0,c=n-1;
+//         while(r<m && c>=0){
+//             if(target == mat[r][c]){
+//                 return true;
+//             }else if(target <mat[r][c]){
+//                 c--;
+//             }else{
+//                 r++;
+//             }
+//         }return false;
+//     } 
