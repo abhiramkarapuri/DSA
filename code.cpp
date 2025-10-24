0486eb64d80cdf0a5a7d6dde8cd9789d1969cb7a
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-10-24 19:34:51 +0530

    added 2sum code

--- code.cpp
+++ code.cpp
@@ -2183,3 +2183,21 @@ using namespace std;
 //         }
 //         return ans;
 //     }
+
+//2 SUM
+// BRUTE FORCE 
+//BETTER
+//OPTIMISED
+vector<int> twoSum(vector<int>& nums, int target) {
+  unordered_map<int,int> m;
+  vector<int> ans;
+  for(int i=0;i<nums.size();i++){
+    int first = nums[i];
+    int sec = target - nums[i];
+      if(m.find(sec)!=m.end()){
+          ans.push_back(i);
+          ans.push_back(m[sec]);
+          break;
+      }m[first] = i;
+  }return ans;
+}
