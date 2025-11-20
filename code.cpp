c22386a4ce490c093d940a2de673374d5536d379
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-11-20 23:35:17 +0530

    added linked list cycle code

--- code.cpp
+++ code.cpp
@@ -2880,3 +2880,19 @@ using namespace std;
 //       }return slow;
 //     }
 // };
+
+//DETECT CYCLE IN LINKED LIST
+// class Solution {
+// public:
+//     bool hasCycle(ListNode *head) {
+        // ListNode *slow = head;
+        // ListNode *fast = head;
+        // while(fast!=NULL && fast->next!=NULL){
+        //     slow = slow->next;
+        //     fast = fast->next->next;
+        //     if(fast == slow){
+        //     return true;
+        // } 
+        // }return false;
+//     }
+// };
