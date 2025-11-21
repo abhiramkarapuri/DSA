b1bbd4e150731565524b8bd5bb6849bb5e363991
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-11-21 20:34:05 +0530

    added double linked list code

--- code.cpp
+++ code.cpp
@@ -2956,56 +2956,55 @@ using namespace std;
 //  };
 
 // DOUBLE LINKED LIST
-class Node{
-public:
-  int data;
-  Node *next;
-  Node *prev;
-  Node(int val){
-    data = val;
-    next = prev = NULL;
-  }
-};
-class DoublyList{
-  Node* head;
-  Node* tail;
-  public:
-  DoublyList(){
-    head = tail = NULL;
-  }
-  void push_front(int val){
-    Node* newNode = new Node(val);
-    if(head == NULL){
-      head = tail = newNode;
-    }else{
-    newNode->next = head;
-    head->prev = newNode;
-    head = newNode;}
-  }
-  void push_back(int val){
-    Node* newNode = new Node(val);
-    if(head == NULL){
-      head = tail = newNode;
-    }else{
-      tail->next = newNode;
-      newNode->prev = tail;
-      tail = newNode;
-    }
-  }
-  void print(){
-    Node* temp = head;
-    while(temp!=NULL){
-      cout<<temp->data<<" <-> ";
-      temp = temp->next;
-    }cout<<"NULL";
-  }
-};
-
-int main(){
-  DoublyList dll;
-  dll.push_front(1);
-  dll.push_front(2);
-  dll.push_front(3);
-  dll.push_back(4);
-  dll.print();
-}
+// class Node{
+// public:
+//   int data;
+//   Node *next;
+//   Node *prev;
+//   Node(int val){
+//     data = val;
+//     next = prev = NULL;
+//   }
+// };
+// class DoublyList{
+//   Node* head;
+//   Node* tail;
+//   public:
+//   DoublyList(){
+//     head = tail = NULL;
+//   }
+//   void push_front(int val){
+//     Node* newNode = new Node(val);
+//     if(head == NULL){
+//       head = tail = newNode;
+//     }else{
+//     newNode->next = head;
+//     head->prev = newNode;
+//     head = newNode;}
+//   }
+//   void push_back(int val){
+//     Node* newNode = new Node(val);
+//     if(head == NULL){
+//       head = tail = newNode;
+//     }else{
+//       tail->next = newNode;
+//       newNode->prev = tail;
+//       tail = newNode;
+//     }
+//   }
+//   void print(){
+//     Node* temp = head;
+//     while(temp!=NULL){
+//       cout<<temp->data<<" <-> ";
+//       temp = temp->next;
+//     }cout<<"NULL";
+//   }
+// };
+// int main(){
+//   DoublyList dll;
+//   dll.push_front(1);
+//   dll.push_front(2);
+//   dll.push_front(3);
+//   dll.push_back(4);
+//   dll.print();
+// }
