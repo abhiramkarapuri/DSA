4cefa2e8cb2246acafb842472a22c5cad420a486
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-11-23 23:06:23 +0530

    added previous smallest element code

--- code.cpp
+++ code.cpp
@@ -3284,21 +3284,40 @@ using namespace std;
 // }cout<<endl;
 // }
 
-//NEXT GREATEST ELEMENT
-int main(){
-  vector<int> arr = {6,8,0,1,3};
-  //next greatest
-  stack<int>s;
-  vector<int>ans(arr.size(),-1);
-  for(int i = arr.size()-1;i>=0;i--){
-    while(s.size()>0 && s.top()<=arr[i]){
-      s.pop();
-    }if(s.empty()){
-      ans[i]=-1;
-    }else{
-      ans[i] = s.top();
-    }s.push(arr[i]);
-  }for(int val : ans){
-    cout<<val<<" ";
-  }
-}
+//NEXT GREATEST ELEMENT //TC =SC = O(N)
+// int main(){
+//   vector<int> arr = {6,8,0,1,3};
+//   //next greatest
+//   stack<int>s;
+//   vector<int>ans(arr.size(),-1);
+//   for(int i = arr.size()-1;i>=0;i--){
+//     while(s.size()>0 && s.top()<=arr[i]){
+//       s.pop();
+//     }if(s.empty()){
+//       ans[i]=-1;
+//     }else{
+//       ans[i] = s.top();
+//     }s.push(arr[i]);
+//   }for(int val : ans){
+//     cout<<val<<" ";
+//   }
+// }
+
+//PREV SMALLEST ELEMENT TC = SC =O(N)
+// int main(){
+//   vector<int> arr = {3,1,0,8,6};
+//   stack<int>s;
+//   vector<int>ans(arr.size(),-1);
+//   for(int i =0;i<arr.size();i++){
+//     while(s.size()>0 && s.top()>=arr[i]){
+//       s.pop();
+//     }if(s.empty()){
+//       ans[i]=-1;
+//     }else{
+//       ans[i] =s.top();
+//     }
+//     s.push(arr[i]);
+//   }for(int val : ans){
+//     cout<<val<<" ";
+//   }
+// }
