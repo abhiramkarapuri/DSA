d294d90b0a18bccc61ba3685dcd3415652cd6cdb
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-12-09 22:29:31 +0530

    added max width of Binary Tree code

--- code.cpp
+++ code.cpp
@@ -3952,24 +3952,25 @@ void postOrder(Node* root){//TC = O(N )
 //   }
 
 //MAX WIDTH OF BINARY TREE
-int widthOfBinaryTree(Node* root) {
-  queue<pair<Node*, unsigned long long>>q;
-  q.push({root,0});
-  int maxWidth =0;
-  while(q.size()>0){
-    int currLevelSize = q.size();
-    unsigned long long stIdx = q.front().second;
-    unsigned long long endIdx = q.back().second;
-    maxWidth = max(maxWidth,(int)(endIdx -stIdx +1));
-    for(int i=0;i<currLevelSize;i++){
-      auto curr = q.front();
-      q.pop();
-      if(curr.first->left){
-        q.push({curr.first->left,curr.second*2+1});
-      }
-      if(curr.first->right){
-        q.push({curr.first->right,curr.second*2+2});
-      }
-    }
-  }return maxWidth;
-}
+// int widthOfBinaryTree(Node* root) {
+//   queue<pair<Node*, unsigned long long>>q;
+//   q.push({root,0});
+//   int maxWidth =0;
+//   while(q.size()>0){
+//     int currLevelSize = q.size();
+//     unsigned long long stIdx = q.front().second;
+//     unsigned long long endIdx = q.back().second;
+//     maxWidth = max(maxWidth,(int)(endIdx -stIdx +1));
+//     for(int i=0;i<currLevelSize;i++){
+//       auto curr = q.front();
+//       q.pop();
+//       if(curr.first->left){
+//         q.push({curr.first->left,curr.second*2+1});
+//       }
+//       if(curr.first->right){
+//         q.push({curr.first->right,curr.second*2+2});
+//       }
+//     }
+//   }return maxWidth;
+// }
+
