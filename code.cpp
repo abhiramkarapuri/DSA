0c8a8bc833fd60134f7fb0d53cf67449c40cf0f2
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-12-11 23:36:36 +0530

    added Validate BST code

--- code.cpp
+++ code.cpp
@@ -4012,23 +4012,23 @@ void postOrder(Node* root){//TC = O(N )
 //   }
 
 //BUILD A BST
-Node* insert(Node* root, int val){
-  if(root == NULL){
-    return new Node(val);
-  }
-  if(val<root->data){
-    root->left = insert(root->left,val);
-  }else{
-    root->right = insert(root->right,val);
-  }return root;
-}
-Node* buildBST(vector<int>arr){
-  Node* root = NULL;
-  for(int val : arr){
-    root = insert(root,val);
-  }
-  return root;
-}
+// Node* insert(Node* root, int val){
+//   if(root == NULL){
+//     return new Node(val);
+//   }
+//   if(val<root->data){
+//     root->left = insert(root->left,val);
+//   }else{
+//     root->right = insert(root->right,val);
+//   }return root;
+// }
+// Node* buildBST(vector<int>arr){
+//   Node* root = NULL;
+//   for(int val : arr){
+//     root = insert(root,val);
+//   }
+//   return root;
+// }
 // //SEARCH IN BST
 // bool search(Node* root,int key){//o(height)
 //   if(root == NULL){
@@ -4099,15 +4099,15 @@ Node* buildBST(vector<int>arr){
 // }
 
 //VALIDATE BST
-bool helper(Node* root,Node* min,Node* max){
-  if(root == NULL) return true;
-  if(min !=NULL && root->data <= min->data){
-    return false;
-  }
-  if(max!= NULL && root->data >= max->data){
-    return false;
-  }return helper(root->left,min,root) && helper(root->right,root,max);
-}
-  bool isValidBST(Node* root) {
-      return helper(root,NULL,NULL);
-  }
+// bool helper(Node* root,Node* min,Node* max){
+//   if(root == NULL) return true;
+//   if(min !=NULL && root->data <= min->data){
+//     return false;
+//   }
+//   if(max!= NULL && root->data >= max->data){
+//     return false;
+//   }return helper(root->left,min,root) && helper(root->right,root,max);
+// }
+//   bool isValidBST(Node* root) {
+//       return helper(root,NULL,NULL);
+//   }
