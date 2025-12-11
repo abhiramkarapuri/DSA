37cdc76192d8d7902fc0aeac8ea84a28880b08cc
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-12-11 22:56:03 +0530

    added BST code

--- code.cpp
+++ code.cpp
@@ -4011,3 +4011,76 @@ void postOrder(Node* root){//TC = O(N )
 //     nextRight = root;
 //   }
 
+//BUILD A BST
+Node* insert(Node* root, int val){
+  if(root == NULL){
+    return new Node(val);
+  }
+  if(val<root->data){
+    root->left = insert(root->left,val);
+  }else{
+    root->right = insert(root->right,val);
+  }return root;
+}
+Node* buildBST(vector<int>arr){
+  Node* root = NULL;
+  for(int val : arr){
+    root = insert(root,val);
+  }
+  return root;
+}
+//SEARCH IN BST
+bool search(Node* root,int key){//o(height)
+  if(root == NULL){
+    return false;
+  }if(root->data == key){
+    return true;
+  }if(key< root->data){
+    return search(root->left,key);
+  }else{
+    return search(root->right,key);
+  }}
+  //DELETE NODE IN BST
+  Node* getInorderSuccessor(Node* root){//left most node in right subtree
+    while(root!=NULL && root->left != NULL){
+      root = root->left;
+    }return root;
+  }
+  Node* delNode(Node* root, int key){//key=>val to delete
+    if(root == NULL){
+      return NULL; 
+    } 
+    if(key<root->data){
+      root->left = delNode(root->left,key);
+    }else if(root->data<key){
+      root->right = delNode(root->right,key);
+    }else{//root = key delNode
+      //O & 1 CHILD CASE
+      if(root->left == NULL){
+        Node* temp = root->right;
+        delete root;
+        return temp;
+      }else if(root->right == NULL){
+        Node* temp = root->left;
+        delete root;
+        return temp;
+      }else{//2CHILD
+        Node* IS = getInorderSuccessor(root->right);
+          root->data = IS->data;
+          root->right = delNode(root->right,IS->data);
+      }
+    }return root;
+  }
+int main(){
+  vector<int> arr = {3,2,1,5,6,4};
+  Node* root = buildBST(arr);
+  cout<<"before : ";
+  inOrder(root);
+  //cout<<search(root,8);
+  cout<<endl;
+  delNode(root,5);
+  cout<<"after :";
+  inOrder(root);
+  cout<<endl;
+  return 0;
+}
