1ddfb153e45a0f7c9b63132f3744c45ad2f84bad
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-12-12 23:29:52 +0530

    added LCA in BST code

--- code.cpp
+++ code.cpp
@@ -4152,3 +4152,81 @@ void postOrder(Node* root){//TC = O(N )
 //     }
 //       return -1;
 //   }
+
+//LOWEST COMMON ANCESTOR IN BST
+// Node* lowestCommonAncestor(Node* root, Node* p, Node* q) {
+//   if(root == NULL)return NULL;
+//   if(root->data >p->data && root->data >q->data){
+//     return lowestCommonAncestor(root->left,p,q);
+//   }
+//   else if (root->data < p->data && root->data < q->data){
+//     return lowestCommonAncestor(root->right,p,q);
+//   }else{
+//     return root;
+//   }
+// }
+
+//CONSTRUCT BST FROM PREORDER //TC = O(N)
+// Node* helper(vector<int>& preorder,int &i,int bound ){
+//   if(i>=preorder.size() || preorder[i]>bound) return NULL;
+//   Node* root = new Node(preorder[i++]);
+//   root->left = helper(preorder,i,root->data);
+//   root->right = helper(preorder,i,bound);
+//   return root;
+// }
+// Node* bstFromPreorder(vector<int>& preorder) {
+//   int i =0;
+//   return helper(preorder,i,INT_MAX);
+// }
+
+//MERGE 2 BST
+// void inorder(Node* root, vector<int> &arr){
+//   if(root ==  NULL) return;
+//   inorder(root->left,arr);
+//   arr.push_back(root->data);
+//   inorder(root->right,arr);
+// }
+// Node* buildBSTFromSorted(vector<int>arr,int st, int end){
+//   if(st>end) return NULL;
+//   int mid = st +(end-st)/2;
+//   Node* root = new Node(arr[mid]);
+//   root->left = buildBSTFromSorted(arr,st,mid-1);
+//   root->right = buildBSTFromSorted(arr,mid+1,end);
+//   return root;
+// }
+// Node* merge2BST(Node* root1, Node* root2){
+//   vector<int>arr1,arr2;
+//   inorder(root1,arr1);
+//   inorder(root2,arr2);
+//   vector<int> temp; // final BST inorder : sorted
+//   int i=0,j=0;
+//   while(i<arr1.size() && j<arr2.size()){
+//     if(arr1[i]<arr2[j]){
+//       temp.push_back(arr1[i++]);
+//     }else{
+//       temp.push_back(arr2[j++]);
+//     }
+//   }while(i<arr1.size()){
+//       temp.push_back(arr1[i++]);
+//     }
+//     while(j<arr2.size()){
+//       temp.push_back(arr2[j++]);
+//     }
+//     return buildBSTFromSorted(temp,0,temp.size()-1);//sorted array,st,end
+// }
+// int main(){
+//   vector<int> arr1 = {8,2,1,10};
+//   vector<int> arr2 ={5,3,0};
+//   Node* root1 = buildBST(arr1); 
+//   Node* root2 = buildBST(arr2); 
+//   Node* root = merge2BST(root1,root2);
+//   vector<int> seq;
+//   inorder(root,seq);
+//   for(int v : seq){
+//     cout<<v<<" ";
+//   }
+//   cout<<endl;
+//   return 0;
+// }
+
+//
