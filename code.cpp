ad640dc0b2d0331b0c3a0ee55c761ae895497c4b
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-12-13 23:29:00 +0530

    added Inorder Pred & Succ in BST code

--- code.cpp
+++ code.cpp
@@ -4322,3 +4322,56 @@ void postOrder(Node* root){//TC = O(N )
 //   }
 // };
 
+//INORDER PREDECESSOR & SUCCESSOR IN BST
+// Node* rightMostInLeftSubtree(Node* root){
+//   Node* ans;
+//   while(root!=NULL){
+//     ans = root;
+//     root = root->right;
+//   }return ans;
+// }
+// Node* leftMostInRightSubtree(Node* root){
+//   Node* ans;
+//   while(root!=NULL){
+//     ans = root;
+//     root = root->left;
+//   }return ans;
+// }
+// vector<int> getPredSucc(Node* root,int key){
+//   Node* curr = root;
+//   Node* pred = NULL;
+//   Node* succ = NULL;
+//   while(curr!=NULL){
+//     if(key<curr->data){
+//       succ = curr;
+//       curr = curr->left;
+//     }else if(key>curr->data){
+//       pred = curr;
+//       curr = curr->right;
+//     }else{
+//       if(curr->left!=NULL){
+//         //inorder pred
+//         pred = rightMostInLeftSubtree(curr->left);
+//       }
+//       if(curr->right!=NULL){
+//         //inorder succ
+//         succ = leftMostInRightSubtree(curr->right);
+//       }
+//       break;
+//     }
+//   }return {pred->data,succ->data};
+// }
+// int main(){
+//   Node* root = new Node(6);
+//   root->left = new Node(4);
+//   root->right = new Node(8);
+//   root->left->left = new Node(1);
+//   root->left->right = new Node(5);
+//   root->right->left = new Node(7);
+//   root->right->right = new Node(9);
+//   int key =7;
+//   vector<int> ans = getPredSucc(root,key);
+//   cout<<"predecessor: "<<ans[0]<<endl;
+//   cout<<"successor: "<<ans[1]<<endl;
+//   return 0;
+// }
