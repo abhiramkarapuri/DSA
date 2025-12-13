c5514eb5892878c11d1db677d2e262674441e507
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-12-13 22:29:01 +0530

    added Populate Next Right Pointers code

--- code.cpp
+++ code.cpp
@@ -4228,3 +4228,74 @@ void postOrder(Node* root){//TC = O(N )
 //   cout<<endl;
 //   return 0;
 // }
+
+//LARGEST BST IN BT
+// class Info{
+//   public:
+//   int min,max,sz;
+//   Info(int mi, int ma, int size){
+//     min = mi;
+//     max = ma;
+//     sz = size;
+//   }
+// };
+// Info helper(Node* root){//TC = O(N)
+//   if(root == NULL){
+//     return Info(INT_MAX,INT_MIN,0);
+//   }
+//   Info left = helper(root->left);
+//   Info right = helper(root->right);
+//   if(root->data > left.max && root->data < right.min){
+//     int currMin = min(root->data,left.min);
+//     int currMax = max(root->data,right.max);
+//     int currSz =  left.sz+right.sz+1;
+//     return Info(currMin,currMax,currSz);
+//   }return Info(INT_MIN,INT_MAX,max(left.sz,right.sz));
+// }
+// int largestBSTinBT(Node* root){
+//   Info info = helper(root);
+//   return info.sz;//max BST size
+// }
+// int main(){
+//   Node* root = new Node(10);
+//   root->left = new Node(5);
+//   root->right = new Node(15);
+//   root->left->left = new Node(1);
+//   root->left->right = new Node(8);
+//   root->right->right = new Node(50);
+//   cout<<largestBSTinBT(root)<<endl;
+//   return 0;
+// }
+
+//POPULATE NEXT RIGHT POINTERS //TC = O(N)
+// Node* connect(Node* root) {
+// if(root == NULL || root->left == NULL){
+//   return root;
+// }
+//   queue<Node*>q;
+//   q.push(root);
+//   q.push(NULL);//mark the end of a level
+//   Node* prev = NULL;
+//   while(q.size()>0){
+//     Node* curr = q.front();
+//     q.pop();
+//     if(curr == NULL){
+//       if(q.size() ==0){
+//         break;
+//       }
+//       q.push(NULL);
+//     }else{
+//       if(curr->left!=NULL){
+//         q.push(curr->left);
+//       }
+//       if(curr->right!=NULL){
+//         q.push(curr->right);
+//       }
+//       if(prev!=NULL){
+//         prev->next = curr;
+//       }
+//     }prev = curr;
+//   }return root;
+// }
+
+//
