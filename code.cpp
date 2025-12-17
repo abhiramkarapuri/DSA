9d70df3212db4cc57616e013f37123b96367526e
abhiramkarapuri <abhiramkarapuri@gmail.com>
2025-12-17 22:00:29 +0530

    added valid palindrome code

--- code.cpp
+++ code.cpp
@@ -1829,6 +1829,27 @@ using namespace std;
 //   }
 
 // VALID PALINDROME
+// bool AlphaNumeric(char ch){
+//   if((ch >='0' && ch <='9') ||
+//     (tolower(ch) >='a' && tolower(ch) <= 'z')){
+//       return true;
+//     }
+//     return false;
+// }
+// bool isPalindrome(string s) {
+//   int st =0, end = s.length()-1;
+//   while(st< end){
+//     if(!AlphaNumeric(s[st])){
+//       st++; continue;
+//     }
+//     if(!AlphaNumeric(s[end])){
+//       end--; continue;
+//     }
+//     if(tolower(s[st]) != tolower(s[end])){
+//       return false;
+//     } st++; end--;
+//   } return true;
+// }
 
 // REMOVE ALL OCCURENCES
 //  string removeOccurrences(string s, string part) {
