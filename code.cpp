5eec6d8be3242a9dcf9c68338eb4144578861de4
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-04 22:37:33 +0530

    added reverse string using 2 pointers code

--- code.cpp
+++ code.cpp
@@ -4986,3 +4986,17 @@ using namespace std;
 //   return result;
 // }
 
+//Reverse a string
+string reverseStr(string s, int k) {
+  int n = s.size();
+  for (int i = 0; i < n; i += 2 * k) {
+    int st = i;
+    int end = min(i + k - 1, n - 1);
+    while (st < end) {
+      swap(s[st], s[end]);
+      st++;
+      end--;
+    }
+  }
+  return s;
+}
