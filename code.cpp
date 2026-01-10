c6c53bd45dad30910bbfc426809c96ae977ee4db
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-10 21:12:20 +0530

    added dp code

--- code.cpp
+++ code.cpp
@@ -5231,40 +5231,40 @@ using namespace std;
 // }
 
 //DP_FIBONACCI
-int fib(int n){
-  if( n <=1){
-    return n;
-  }
-  int prev = 1; // n-1
-  int prev2 = 0; //n-2 
-  int curr;
-  for(int i=2;i<=n;i++){
-    curr = prev + prev2;
-    prev2 = prev;
-    prev = curr;
-  }
-  return prev;
-}
-int main(){
-  int n =5;
-  cout<<fib(7);
-}
+// int fib(int n){
+//   if( n <=1){
+//     return n;
+//   }
+//   int prev = 1; // n-1
+//   int prev2 = 0; //n-2 
+//   int curr;
+//   for(int i=2;i<=n;i++){
+//     curr = prev + prev2;
+//     prev2 = prev;
+//     prev = curr;
+//   }
+//   return prev;
+// }
+// int main(){
+//   int n =5;
+//   cout<<fib(7);
+// }
 
 // Function to calculate Fibonacci using memoization
-    int fib(int n, vector<int>& dp) {
-        // If base case return n
-        if (n <= 1) return n;
-        // If already computed, return stored value
-        if (dp[n] != -1) return dp[n];
-        // Otherwise compute and store
-        dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
-        return dp[n];
-    }
-int main() {
-    int n = 10;
-    vector<int> dp(n + 1, -1);
-    cout << fib(n, dp);
-    return 0;
-}
+// int fib(int n, vector<int>& dp) {
+//     // If base case return n
+//     if (n <= 1) return n;
+//     // If already computed, return stored value
+//     if (dp[n] != -1) return dp[n];
+//     // Otherwise compute and store
+//     dp[n] = fib(n - 1, dp) + fib(n - 2, dp);
+//     return dp[n];
+//   }
+// int main() {
+//     int n = 10;
+//     vector<int> dp(n + 1, -1);
+//     cout << fib(n, dp);
+//     return 0;
+// }
 
 //g++ -std=c++11 code.cpp && a.exe
