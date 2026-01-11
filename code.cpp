e701f4193227c8e08507367ca640cdab54d0822d
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-11 20:28:59 +0530

    added Climbing Stairs code

--- code.cpp
+++ code.cpp
@@ -5267,4 +5267,21 @@ using namespace std;
 //     return 0;
 // }
 
+//CLIMBING STAIRS
+// int climbStairs(int n) {
+//     if (n <= 1) return 1;
+//     int prev1 = 1, prev2 = 1;
+//     for (int i = 2; i <= n; i++) {
+//         int curr = prev1 + prev2;
+//         prev2 = prev1;
+//         prev1 = curr;
+//     }
+//     return prev1;
+// }
+// int main() {
+//     int n = 5;
+//     cout << climbStairs(n);
+//     return 0;
+// }
+
 //g++ -std=c++11 code.cpp && a.exe
