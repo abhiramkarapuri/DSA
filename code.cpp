36df50d44c7f8459ed109a011fda5173709e37ca
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-12 21:57:19 +0530

    added Frog Jump with K distance code

--- code.cpp
+++ code.cpp
@@ -5305,4 +5305,27 @@ using namespace std;
 //     return 0;
 // }
 
+//FROG JUMP WITH K DISTANCE
+// int frogJump(int n, int k, vector<int>& height) {
+//     vector<int> dp(n, INT_MAX);
+//     dp[0] = 0;
+//     for (int i = 1; i < n; i++) {
+//         for (int j = 1; j <= k; j++) {
+//             if (i - j >= 0) {
+//                 dp[i] = min(dp[i],dp[i - j] + abs(height[i] - height[i - j]));
+//             }
+//         }
+//     }
+//     return dp[n - 1];
+// }
+// int main() {
+//     int n, k;
+//     cin >> n >> k;
+//     vector<int> height(n);
+//     for (int i = 0; i < n; i++)
+//         cin >> height[i];
+//     cout << frogJump(n, k, height);
+//     return 0;
+// }
+
 //g++ -std=c++11 code.cpp && a.exe
