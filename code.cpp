f8c9ab3339d5f94b269077e1c6dd485d16765e74
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-14 21:45:07 +0530

    added Grid Unique Paths 2 code

--- code.cpp
+++ code.cpp
@@ -5388,7 +5388,6 @@ using namespace std;
 //GRID UNIQUE PATHS
 // int uniquePaths(int m, int n) {
 //     vector<vector<int>> dp(m, vector<int>(n, 1));
-
 //     for (int i = 1; i < m; i++) {
 //         for (int j = 1; j < n; j++) {
 //             dp[i][j] = dp[i-1][j] + dp[i][j-1];
@@ -5396,4 +5395,25 @@ using namespace std;
 //     }
 //     return dp[m-1][n-1];
 // }
+
+//GRID UNIQUE PATHS 2
+int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+    int m = obstacleGrid.size();
+    int n = obstacleGrid[0].size();
+    if (obstacleGrid[0][0] == 1) return 0;
+    vector<vector<long long>> dp(m, vector<long long>(n, 0));
+    dp[0][0] = 1;
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            if (obstacleGrid[i][j] == 1) {
+                dp[i][j] = 0;
+            } else {
+                if (i > 0) dp[i][j] += dp[i - 1][j];
+                if (j > 0) dp[i][j] += dp[i][j - 1];
+            }
+        }
+    }
+    return dp[m - 1][n - 1];
+}
+
 //g++ -std=c++11 code.cpp && a.exe
