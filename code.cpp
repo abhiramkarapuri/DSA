b284f5c7d9297fc1dd44cfc3371b0f3f7bd41281
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-14 21:43:11 +0530

    added Grid Unique Paths code

--- code.cpp
+++ code.cpp
@@ -5385,5 +5385,15 @@ using namespace std;
 //     return prev[3];
 // }
 
+//GRID UNIQUE PATHS
+// int uniquePaths(int m, int n) {
+//     vector<vector<int>> dp(m, vector<int>(n, 1));
 
+//     for (int i = 1; i < m; i++) {
+//         for (int j = 1; j < n; j++) {
+//             dp[i][j] = dp[i-1][j] + dp[i][j-1];
+//         }
+//     }
+//     return dp[m-1][n-1];
+// }
 //g++ -std=c++11 code.cpp && a.exe
