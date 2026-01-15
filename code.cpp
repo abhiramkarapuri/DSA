a06e59ce0690b6c1ac732567b752815dede201a5
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-15 21:06:58 +0530

    added triangle code

--- code.cpp
+++ code.cpp
@@ -5437,4 +5437,20 @@ using namespace std;
 //     return dp[m-1][n-1];
 // }
 
+//TRIANGLE
+// int minimumTotal(vector<vector<int>>& triangle) {
+//     int n = triangle.size();
+//     vector<vector<int>> dp = triangle;
+//     for (int i = 1; i < n; i++) {
+//         for (int j = 0; j <= i; j++) {
+//             if (j == 0)
+//                 dp[i][j] += dp[i-1][j];
+//             else if (j == i)
+//                 dp[i][j] += dp[i-1][j-1];
+//             else
+//                 dp[i][j] += min(dp[i-1][j-1], dp[i-1][j]);
+//         }
+//     }
+//     return *min_element(dp[n-1].begin(), dp[n-1].end());
+// }
 //g++ -std=c++11 code.cpp && a.exe
