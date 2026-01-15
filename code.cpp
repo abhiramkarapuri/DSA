17913d6606d66cc6058f132bac70c47d426278c9
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-15 21:01:30 +0530

    added code

--- code.cpp
+++ code.cpp
@@ -5416,4 +5416,25 @@ using namespace std;
 //     return dp[m - 1][n - 1];
 // }
 
+//MINIMUM PATH SUM IN GRID
+// int minPathSum(vector<vector<int>>& grid) {
+//     int m = grid.size();
+//     int n = grid[0].size();
+//     vector<vector<int>> dp(m, vector<int>(n, 0));
+//     dp[0][0] = grid[0][0];
+//     // First column
+//     for (int i = 1; i < m; i++)
+//         dp[i][0] = dp[i-1][0] + grid[i][0];
+//     // First row
+//     for (int j = 1; j < n; j++)
+//         dp[0][j] = dp[0][j-1] + grid[0][j];
+//     // Fill the rest
+//     for (int i = 1; i < m; i++) {
+//         for (int j = 1; j < n; j++) {
+//             dp[i][j] = grid[i][j] + min(dp[i-1][j], dp[i][j-1]);
+//         }
+//     }
+//     return dp[m-1][n-1];
+// }
+
 //g++ -std=c++11 code.cpp && a.exe
