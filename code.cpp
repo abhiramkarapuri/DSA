7fc18cd2e1c5841c2e626548bf2e29b365e131e7
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-20 22:12:25 +0530

    added 0/1 Knapsack code

--- code.cpp
+++ code.cpp
@@ -5633,5 +5633,28 @@ using namespace std;
 //     return 0;
 // }
 
+//0/1 KNAPSACK
+// int knapsack(int W, vector<int>& wt, vector<int>& val, int n) {
+//     vector<vector<int>> dp(n + 1, vector<int>(W + 1, 0));
+//     for (int i = 1; i <= n; i++) {
+//         for (int w = 1; w <= W; w++) {
+//             if (wt[i - 1] <= w) {
+//                 dp[i][w] = max(
+//                     val[i - 1] + dp[i - 1][w - wt[i - 1]],
+//                     dp[i - 1][w]
+//                 );
+//             } else {
+//                 dp[i][w] = dp[i - 1][w];
+//             }
+//         }
+//     }
+//     return dp[n][W];
+// }
+// int main() {
+//     vector<int> wt = {1, 3, 4, 5};
+//     vector<int> val = {1, 4, 5, 7};
+//     int W = 7;
+//     cout << "Maximum value = " << knapsack(W, wt, val, wt.size()) << endl;
+// }
 
 //g++ -std=c++11 code.cpp && a.exe
