7ba98965c48091a680ae94ab932b2c3d2f56b68a
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-23 21:32:49 +0530

    added Coin Change 2 code

--- code.cpp
+++ code.cpp
@@ -5706,4 +5706,16 @@ using namespace std;
 //     return 0;
 // }
 
+//COIN CHANGE 2
+// int change(int amount, vector<int>& coins) {
+//         vector<int> dp(amount + 1, 0);
+//         dp[0] = 1;
+//         for (int coin : coins) {
+//             for (int i = coin; i <= amount; i++) {
+//                 dp[i] += dp[i - coin];
+//             }
+//         }
+//         return dp[amount];
+//     }
+
 //g++ -std=c++11 code.cpp && a.exe
