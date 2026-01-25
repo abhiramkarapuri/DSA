127427a386598c572e3c2fbd8062625efe93119f
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-25 23:13:14 +0530

    added Longest Common Substring code

--- code.cpp
+++ code.cpp
@@ -5773,4 +5773,23 @@ using namespace std;
 //     return 0;
 // } 
 
+//LONGEST COMMON SUBSTRING
+// int longestCommonSubstring(string s1, string s2) {
+//     int n = s1.size(), m = s2.size();
+//     vector<int> prev(m + 1, 0), curr(m + 1, 0);
+//     int ans = 0;
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= m; j++) {
+//             if (s1[i - 1] == s2[j - 1]) {
+//                 curr[j] = prev[j - 1] + 1;
+//                 ans = max(ans, curr[j]);
+//             } else {
+//                 curr[j] = 0;
+//             }
+//         }
+//         swap(prev, curr);
+//     }
+//     return ans;
+// }
+
 //g++ -std=c++11 code.cpp && a.exe
