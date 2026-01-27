ba134f6f8197665ac094e690cc45e727e83d6e36
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-27 22:51:59 +0530

    added Shortest common Supersequence code

--- code.cpp
+++ code.cpp
@@ -5857,6 +5857,44 @@ using namespace std;
 //     return deletions + insertions;
 // }
 
-
+//SHORTEST COMMON SUPERSEQUENCE
+// vector<vector<int>> lcsTable(string &A, string &B) {
+//     int n = A.size(), m = B.size();
+//     vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//     for (int i = 1; i <= n; i++) {
+//         for (int j = 1; j <= m; j++) {
+//             if (A[i - 1] == B[j - 1])
+//                 dp[i][j] = 1 + dp[i - 1][j - 1];
+//             else
+//                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+//         }
+//     }
+//     return dp;
+// }
+// string shortestCommonSupersequence(string A, string B) {
+//     int n = A.size(), m = B.size();
+//     auto dp = lcsTable(A, B);
+//     int i = n, j = m;
+//     string ans = "";
+//     while (i > 0 && j > 0) {
+//         if (A[i - 1] == B[j - 1]) {
+//             ans.push_back(A[i - 1]);
+//             i--; j--;
+//         } 
+//         else if (dp[i - 1][j] > dp[i][j - 1]) {
+//             ans.push_back(A[i - 1]);
+//             i--;
+//         } 
+//         else {
+//             ans.push_back(B[j - 1]);
+//             j--;
+//         }
+//     }
+//     // Add remaining characters
+//     while (i > 0) ans.push_back(A[--i]);
+//     while (j > 0) ans.push_back(B[--j]);
+//     reverse(ans.begin(), ans.end());
+//     return ans;
+// }
 
 //g++ -std=c++11 code.cpp && a.exe
