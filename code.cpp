ab1ee2da76b4629db89410794446500144d44bf7
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-28 22:51:24 +0530

    added Distinct Subsequences code

--- code.cpp
+++ code.cpp
@@ -5897,4 +5897,22 @@ using namespace std;
 //     return ans;
 // }
 
+//DISTINCT SUBSEQUENCES
+long long numDistinct(string s, string t) {
+        int n = s.size(), m = t.size();
+        vector<vector<long long>> dp(n + 1, vector<long long>(m + 1, 0));
+        // base case: empty t
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (s[i - 1] == t[j - 1]) {
+                    dp[i][j] += dp[i - 1][j - 1];
+                }
+            }
+        }
+        return dp[n][m];
+    }
 //g++ -std=c++11 code.cpp && a.exe
