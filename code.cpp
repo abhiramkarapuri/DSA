0172ee3cb0ad05f7d534aea335278ae9b1814449
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-01-28 22:53:22 +0530

    added Edit Distance code

--- code.cpp
+++ code.cpp
@@ -5916,5 +5916,28 @@ using namespace std;
 //         return dp[n][m];
 //     }
 
+//EDIT DISTANCE
+// int minDistance(string word1, string word2) {
+//         int n = word1.size(), m = word2.size();
+//         vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+//         for (int i = 0; i <= n; i++)
+//             dp[i][0] = i;
+//         for (int j = 0; j <= m; j++)
+//             dp[0][j] = j;
+//         for (int i = 1; i <= n; i++) {
+//             for (int j = 1; j <= m; j++) {
+//                 if (word1[i - 1] == word2[j - 1]) {
+//                     dp[i][j] = dp[i - 1][j - 1];
+//                 } else {
+//                     dp[i][j] = 1 + min({
+//                         dp[i - 1][j],     // delete
+//                         dp[i][j - 1],     // insert
+//                         dp[i - 1][j - 1]  // replace
+//                     });
+//                 }
+//             }
+//         }
+//         return dp[n][m];
+//     }
 
 //g++ -std=c++11 code.cpp && a.exe
