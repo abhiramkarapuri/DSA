319adefbafe066db254c9bc40285bc0c07f1c467
abhiramkarapuri <abhiramkarapuri@gmail.com>
2026-02-04 23:19:15 +0530

    added Partition array for max sum code

--- code.cpp
+++ code.cpp
@@ -6227,5 +6227,21 @@ using namespace std;
 //   return dp[n-1];
 // }
 
+//PARTITION ARRAY FOR MAX SUM
+// int maxSumAfterPartitioning(vector<int>& arr, int k) {
+//   int n = arr.size();
+//   vector<int> dp(n + 1, 0);  // dp[n] = 0 
+//   for (int i = n - 1; i >= 0; i--) {
+//     int maxVal = 0;
+//     int best = 0;
+//     for (int j = i; j < n && j < i + k; j++) {
+//         maxVal = max(maxVal, arr[j]);
+//         int len = j - i + 1;
+//         best = max(best, maxVal * len + dp[j + 1]);
+//     }   
+//     dp[i] = best;
+//   }
+//   return dp[0];
+// }
 
 //g++ -std=c++11 code.cpp && a.exe
